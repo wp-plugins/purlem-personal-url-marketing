835f23afae9fc930a22d969158818efdcc1d11d6
mthomas9 <mthomas9@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-02 18:10:40 +0000

    Added version 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/purlem-personal-url-marketing/trunk@647117 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- purlem.php
+++ purlem.php
@@ -3,7 +3,7 @@
 Plugin Name: Purlem Personalized URL
 Plugin URI: http://purlem.com
 Description: Personalize your blog to visitors and track results with Personalized URLs (PURLs). <strong>The Plugin Requires a <a href='http://www.purlem.com'>Purlem Account</a>.</strong>
-Version: 1.1.9
+Version: 1.2.0
 Author: Marty Thomas
 Author URI: http://purlem.com/company
 License: A "Slug" license name e.g. GPL2
